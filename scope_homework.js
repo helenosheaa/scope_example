1658715acb5f2f8cfeda536647abc1ea762069c4
helenosheaa <helenosheaa@gmail.com>
2018-07-31 08:23:12 +0100

    MVP

---
+++ scope_homework.js
@@ -0,0 +1,232 @@
+#### Episode 1
+
+```js
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+The murderer is Miss Scarlet.
+
+The murderer is assigned as Miss Scarlet which has a block scope so can be refered to below.
+
+#### Episode 2
+
+```js
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+You should not change a const variable. It will throw an error.
+TypeError: Assignment to constant variable.
+
+#### Episode 3
+
+```js
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+```
+
+First Verdict: The murderer is Mrs. Peacock.
+Second Verdict: The murderer is Professor Plum.
+
+A let variable can be reassigned. So in the first case it will be changed. In the second the block scope will make it the first variable.
+
+#### Episode 4
+
+```js
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+```
+
+The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.
+As it is inside declareAllSuspects suspect three will be reassigned to Colonel Mustard.
+Suspect three is still Mrs. Peacock as it has only been changed within the method.
+
+#### Episode 5
+
+```js
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+```
+
+The weapon is the Revolver.
+The changeWeapon method alters the weapon to be a revolver which can be seen below in the declareWeapon.
+
+#### Episode 6
+
+```js
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+The murderer is Mrs. White.
+The changeMurderer function is able to change the murderer as it is a let property to begin with, the first change doesn't have a type cast, so the plotTwist which is a const function when it's called overwrites the murderer from the start.
+
+
+#### Episode 7
+
+```js
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+The murderer is Mr Green.
+The murderer is changed as a const to Mr Green at the start of the change murderer function.
+
+#### Episode 8
+
+```js
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+```
+
+The weapon is Candle Stick.
+The room was changed to the dining room which changed the murderer to Colonel Mustard. Where the weapon was assigned to Candle Stick.
+
+#### Episode 9
+
+```js
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+The murderer is Professor Plum.
+The murderer is declared as Professor Plum on the top line. The if statement is not named or called below so it does not change the murderer.
